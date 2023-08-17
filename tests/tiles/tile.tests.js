003ab5012990cbeca315013112274f4955cd487f
Chris Moriarty <chris@new-host-4.home>
2023-08-17 11:51:23 -0400

    check tile matches to istop/isbottom tests

--- tests/tiles/tile.tests.js
+++ tests/tiles/tile.tests.js
@@ -1,11 +1,22 @@
-const { tiles } = require("../../tiles")
+const { tiles } = require("../../tiles");
 
+test("Test that tiles with top matches have isBottom equal to true", () => {
+	for (tile of tiles) {
+		if (tile.topMatches.length) expect(tile.isBottom).toBe(true);
+	}
+});
 
-test('test is Top and has top score', () => {
-    for(tile of tiles){
-      console.log(tile)
-      if(tile.topMatches.length)
-      expect(tile.isTop).toBe(true);
-    }
-    
-  });
+test("Test that tiles with bottom matches have isTop equal to true", () => {
+	for (tile of tiles) {
+		if (tile.bottomMatches.length) expect(tile.isTop).toBe(true);
+	}
+});
+
+test("Test that tiles with bottom matches and top matches have isBottom and isTop equal to true", () => {
+	for (tile of tiles) {
+		if (tile.bottomMatches.length && tile.topMatches.length) {
+			expect(tile.isBottom).toBe(true);
+			expect(tile.isTop).toBe(true);
+		}
+	}
+});
